025143b49d6b157b59afb68a6e9e0c781136ec56
Unknown <jensbern@kth.se>
2018-06-18 11:12:17 +0200

    No inputLabel if input has no argument, print transparent if no argument

--- kjell/Assets/Kjell/Scripts/Output.cs
+++ kjell/Assets/Kjell/Scripts/Output.cs
@@ -6,6 +6,18 @@ namespace Kjell
 	public class Output : MonoBehaviour
 	{
 		public Text Text;
+        public Image Image;
+
+        private void Start()
+        {
+            // Change the transparency of the output message (0 is transparent, 1 is opaque)
+            if (Text.text == "")
+            {
+                var tempColor = Image.color;
+                tempColor.a = 0.3f; 
+                Image.color = tempColor;
+            }
+        }
 	}
 }
 
