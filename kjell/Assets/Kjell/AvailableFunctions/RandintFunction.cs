d592bda5c44296fec4c18c6fb22020b20955dcb4
Jonathan Bäckström <jonathan.backstrom95@gmail.com>
2018-06-28 16:53:47 +0200

    Create and test randint function

---
+++ kjell/Assets/Kjell/AvailableFunctions/RandintFunction.cs
@@ -0,0 +1,33 @@
+﻿using Compiler;
+using UnityEngine;
+
+public class RandintFunction : Function
+{
+	public RandintFunction()
+	{
+		name = "randint";
+		buttonText = "randint()";
+		inputParameterAmount.Add(2);
+		hasReturnVariable = true;
+		pauseWalker = false;
+	}
+
+	public override Variable runFunction(Scope currentScope, Variable[] inputParas, int lineNumber)
+	{
+		foreach (var parameter in inputParas)
+		{
+			if (parameter.variableType != VariableTypes.number || parameter.getNumber() != (int)parameter.getNumber())
+				PMWrapper.RaiseError(lineNumber, "Fel datatyp. Parametrarna till randint får bara vara heltal.");
+		}
+
+		var start = (int) inputParas[0].getNumber();
+		var end = (int)inputParas[1].getNumber();
+
+		if (start > end)
+			PMWrapper.RaiseError("Fel i parametrarna till randint. Första parametern ska vara mindre än den andra");
+
+		var randomNumber = Random.Range(start, end+1);
+
+		return new Variable("random", randomNumber);
+	}
+}
