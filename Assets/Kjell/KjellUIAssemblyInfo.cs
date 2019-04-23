93e38aff3c0dd26a7cff89fc7d11fdd87e70df75
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-23 12:17:21 +0200

    Upped version to 1.1.0

--- Assets/Kjell/KjellUIAssemblyInfo.cs
+++ Assets/Kjell/KjellUIAssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using PM;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 namespace Kjell
 {
