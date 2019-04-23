14ebf93d452165f59623af61e95dd8f6492230ef
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-23 11:07:43 +0200

    Added Kjell UI AssemblyInfo to Kjell prefab

---
+++ Assets/Kjell/KjellUIAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using PM;
+using UnityEngine;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+public class KjellUIAssemblyInfo : AssemblyInfo
+{
+    public override bool canBeEdited => true;
+}
